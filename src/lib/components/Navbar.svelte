dace6d1a21d8c7c9d6bbdbecc0795f23ec1fa471
Vichard14u <58327968+Vichard14u@users.noreply.github.com>
2022-03-20 11:47:27 +0100

    commit 1

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,51 @@
+<nav
+    class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between px-4"
+>
+    <div>
+        <a class="navbar-brand" href="/">Navbar</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon" />
+        </button>
+    </div>
+    <div class="" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    Réponses par niveau d'expertise
+                </a>
+                <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                >
+                    <li><a class="dropdown-item" href="/reponses-par-niveau-dexpertise/globales">Globales</a></li>
+                    <li>
+                        <a class="dropdown-item" href="/reponses-par-niveau-dexpertise/individuelles">Individuelles</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/comparatif-des-réponses-selon-la-region">Comparatif des réponses selon la région
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/choix-supplementaire">Choix supplémentaire
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import '../styles/global.scss'
+    import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar/>
+<main class="container">
+    <slot/>
+</main>
+
